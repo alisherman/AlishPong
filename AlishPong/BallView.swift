7de25e4a11570d9c5d0af824950a77117f396d6a
Alish Erman <alisherman@Alishs-MacBook-Pro.local>
2014-11-19 19:27:07 -0500

    initial import

---
+++ AlishPong/BallView.swift
@@ -0,0 +1,15 @@
+//
+//  BallView.swift
+//  AlishPong
+//
+//  Created by Alish Erman on 11/19/14.
+//  Copyright (c) 2014 Alish Erman. All rights reserved.
+//
+
+import UIKit
+
+class BallView: UIView {
+
+   
+
+}
--- AlishPong/ViewController.swift
+++ AlishPong/ViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ballView = BallView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blackColor()
+        self.setupBallView()
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupBallView() {
+        self.ballView.frame = CGRect(x: 80, y: 80, width: 20, height: 20)
+        self.ballView.center = self.view.center
+        self.ballView.backgroundColor = UIColor.greenColor()
+        self.ballView.layer.cornerRadius = 10
+        self.view.addSubview(self.ballView)
     }
 
 
